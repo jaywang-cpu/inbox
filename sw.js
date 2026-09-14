214f0d5ab29156f5f034b4a70a46d8f9f69a778a
JayWang <myName@email.com>
2026-09-14 19:57:02 -0400

    四象限加锁：手动放过的位置自动上锁，🔒 点一下解锁回到自动判断

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'inbox-v15';
+const C = 'inbox-v16';
 const FILES = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
