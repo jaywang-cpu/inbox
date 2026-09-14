a50a2b6a6ddcf46ed3bc1f3addf2864b385734e3
JayWang <myName@email.com>
2026-09-14 19:39:47 -0400

    日历：格子加大，直接显示事情标题（每格最多2条+溢出计数）

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C = 'inbox-v12';
+const C = 'inbox-v13';
 const FILES = ['./index.html','./manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(C).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
